894784a8683476eb99e1ed00f2ca7887e74310d9
Dan Bentley <dan@numiko.com>
2021-01-29 15:00:51 +0000

    Add Wagtail

---
+++ config/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '#8ee3-hpaw+_enm-^20irgts6i*f88$j*cw+tnew!)6ln-94r9'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
